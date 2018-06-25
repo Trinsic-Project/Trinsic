a8370e2fc4dd6080dadd036232693ce0460d80ae
Jacob Feinberg <JacobFeinberg92@gmail.com>
2018-06-25 17:10:32 -0400

    (UPDATE): Make contract variables public

--- contracts/SwapAgreement.sol
+++ contracts/SwapAgreement.sol
@@ -4,10 +4,11 @@ pragma solidity ^0.4.17;
 contract SwapAgreement {
 
 // Declares variables that will be used to store addresses and skilss for each party involved in swap
-    address initiator;
-    address respondent;
-    string initiatorSkill;
-    string respondentSkill;
+//The public keyword here creates getter methods i.e. initiator() { return initiator }
+    address public initiator;
+    address public respondent;
+    string public initiatorSkill;
+    string public respondentSkill;
 
 // Set the address and skill for the person who initiates the swap, this function is invoked as soon as the contract is compiled
     constructor() public {
@@ -27,23 +28,4 @@ contract SwapAgreement {
         respondentSkill = "JavaScript";
     }
     
-//Get initiator address
-    function GetInitiator() public view returns (address) {
-        return initiator;            
-    }
-
-//Get respondent address    
-    function GetRespondent() public view returns (address) {
-        return respondent;            
-    }
-    
-//Get Initiator Skill information 
-    function GetInitiatorSkill() public view returns (string) {
-        return initiatorSkill;            
-    }
-    
-//Get Respondent Skill information 
-    function GetRespondentSkill() public view returns (string) {
-        return respondentSkill;            
-    }
 }
