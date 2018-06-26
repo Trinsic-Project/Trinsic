9352629221060742c2047658f20d801b4adcd218
Jacob Feinberg <JacobFeinberg92@gmail.com>
2018-06-26 12:05:07 -0400

    (CREATE): Add Finalize Button

--- client/components/Initiate-Contract.js
+++ client/components/Initiate-Contract.js
@@ -1,6 +1,6 @@
 import React, {Component} from 'react'
 import {connect} from 'react-redux'
-import {fetchWeb3, fetchContract} from '../store';
+import {fetchWeb3, fetchContract, finalizeContractThunk} from '../store';
 
 class InitiateContract extends Component{
   
@@ -13,6 +13,7 @@ class InitiateContract extends Component{
         <div>
           <h3>Initiate Contract</h3>
           <button name='initiate-contract' onClick={() => this.props.fetch(this.props.web3)}>Click Here to Initiate</button>
+          <button name='finalize-contract' onClick={() => this.props.finalize(this.props.contract)}>Click Here to Finalize</button>
         </div>
     )
   }
@@ -21,13 +22,15 @@ class InitiateContract extends Component{
 const mapState = state => {
   return {
     web3: state.web3,
+    contract: state.contract
   }
 }
 
 const mapDispatch = dispatch => {
 	return {
 		initiate: () => dispatch(fetchWeb3()), 
-    fetch: (web3) => dispatch(fetchContract(web3))
+    fetch: (web3) => dispatch(fetchContract(web3)),
+    finalize: (contract) => dispatch(finalizeContractThunk(contract))
 	}
 }
 
--- client/store/contract.js
+++ client/store/contract.js
@@ -5,9 +5,12 @@ const contractInstance = contract(SwapAgreement) //This creates an instance of o
 
 //Action Types
 const GET_CONTRACT = 'GET_CONTRACT'
+const FINALIZE_CONTRACT = 'FINALIZE_CONTRACT' 
 
 //Action Creators
 const getContract = contract => ({type: GET_CONTRACT, payload: contract})
+const finalizeContract = contract => ({type: FINALIZE_CONTRACT, payload: contract})
+
 
 //Thunk Creators
 export const fetchContract = web3 => {
@@ -15,14 +18,34 @@ export const fetchContract = web3 => {
   return dispatch => {
     contractInstance
       .deployed()
-      .then(instance => dispatch(getContract(instance)))
+      .then(instance => {
+        console.log(instance.initiatorSkill.call());
+        console.log(instance.initiator.call());
+        dispatch(getContract(instance))
+      })
   }
 }
+
+export const finalizeContractThunk = contract => {
+  return dispatch => {
+    console.log(contract)
+    contract.FinalizeAgreement()
+      .then(instance => {
+        console.log(instance.respondentSkill.call());
+        console.log(instance.respondent.call());
+        dispatch(finalizeContract(instance))
+      })
+  }
+}
+
+
 
 //Reducer
 export default function(state = {}, action) {
   switch (action.type) {
     case GET_CONTRACT:
+      return action.payload
+    case FINALIZE_CONTRACT:
       return action.payload
     default:
       return state
