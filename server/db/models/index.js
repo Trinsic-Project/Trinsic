93c89d6178d7587db6e66e3aba9522dc30e64f9f
Allan James Lapid <ajLapid718@gmail.com>
2018-06-27 12:46:19 -0400

    (ASSOC): Set up message-user associations

--- server/db/models/index.js
+++ server/db/models/index.js
@@ -1,18 +1,10 @@
 const User = require('./user')
+const Message = require('./Message.js');
 
-/**
- * If we had any associations to make, this would be a great place to put them!
- * ex. if we had another model called BlogPost, we might say:
- *
- *    BlogPost.belongsTo(User)
- */
+Message.belongsTo(User);
+User.hasMany(Message);
 
-/**
- * We'll export all of our models here, so that any time a module needs a model,
- * we can just require it from 'db/models'
- * for example, we can say: const {User} = require('../db/models')
- * instead of: const User = require('../db/models/user')
- */
 module.exports = {
-  User
+  User,
+  Message
 }
