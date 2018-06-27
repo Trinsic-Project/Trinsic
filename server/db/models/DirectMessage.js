cb348df99cb7cc35384f85eb7a3150186660c289
Allan James Lapid <ajLapid718@gmail.com>
2018-06-27 12:56:03 -0400

    (ASSOC): Set up dm and user associations

--- server/db/models/DirectMessage.js
+++
@@ -1,6 +0,0 @@
-const Sequelize = require('sequelize');
-const db = require('../db.js');
-
-const DirectMessage = db.define('directMessage');
-
-module.exports = DirectMessage;
--- server/db/models/index.js
+++ server/db/models/index.js
@@ -1,10 +1,18 @@
 const User = require('./user')
 const Message = require('./Message.js');
+const DirectMessageChat = require('./DirectMessageChat.js');
 
 Message.belongsTo(User);
 User.hasMany(Message);
 
+DirectMessageChat.hasMany(Message);
+Message.belongsTo(DirectMessageChat);
+
+DirectMessageChat.belongsToMany(User, {through: 'negotiation'});
+User.belongsToMany(DirectMessageChat, {through: 'negotiation'});
+
 module.exports = {
   User,
-  Message
+  Message,
+  DirectMessageChat
 }
