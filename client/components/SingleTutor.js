4cba158cd81f05f0d6892999e396259139fbdccf
Lindsay Sack <lindsay.sack@gmail.com>
2018-06-28 19:53:09 -0400

    (UPDATE): Add material-ui styling to single tutor

--- client/components/SingleTutor.js
+++ client/components/SingleTutor.js
@@ -1,60 +1,6 @@
-// import React, {Component} from 'react'
-// import {connect} from 'react-redux'
-// import {fetchSingleTutor} from '../store'
-
-// class SingleTutor extends Component {
-//   componentDidMount() {
-//     this.props.fetchTutor(1)
-//   }
-
-//   render() {
-//     return (
-//       <div>
-//       <div>
-//         <h3>{this.props.tutor.fullName}</h3>
-//         <img
-//           src={this.props.tutor.imageUrl}
-//         />
-//                 <div>
-//           <h4>Location</h4>
-//           <p>{this.props.tutor.city + ', ' + this.props.tutor.state}</p>
-//         </div>
-//         <div>
-//           <h4>Bio</h4>
-//           <p>{this.props.tutor.biography}</p>
-//         </div>
-//         <div>
-//           <h4>Skills</h4>
-//           <p>No skills to display</p>
-//         </div>
-//         <div>
-//           <h4>Swap History</h4>
-//           <p>No past swaps to display</p>
-//         </div>
-//       </div>
-//     </div>
-//     )
-//   }
-// }
-
-// const mapStateToProps = state => {
-//   return {
-//     tutor: state.tutor
-//   }
-// }
-
-// const mapDispatchToProps = dispatch => {
-//   return {
-//     fetchTutor: tutor => dispatch(fetchSingleTutor(tutor))
-//   }
-// }
-
-// export default connect(mapStateToProps, mapDispatchToProps)(SingleTutor)
-
-
 import React, {Component} from 'react'
 import PropTypes from 'prop-types'
-import compose from 'recompose/compose';
+import compose from 'recompose/compose'
 import {withStyles} from '@material-ui/core/styles'
 import Card from '@material-ui/core/Card'
 import CardActions from '@material-ui/core/CardActions'
@@ -69,12 +15,11 @@ import {InitiateContract} from './'
 const styles = {
   card: {
     maxWidth: 375,
-    direction: 'row',
-    justify: 'center',
+    margin: 'auto',
+    minHeight: 560,
   },
   media: {
-    height: 0,
-    paddingTop: '56.25%' // 16:9
+    paddingTop: '70%' // 16:9
   }
 }
 
@@ -119,18 +64,16 @@ const mapStateToProps = state => {
       tutor: state.tutor
     }
   }
-  
+
   const mapDispatchToProps = dispatch => {
     return {
       fetchTutor: tutor => dispatch(fetchSingleTutor(tutor))
     }
   }
-  
+
   SingleTutor.propTypes = {
     classes: PropTypes.object.isRequired
   }
-
-  // export default connect(mapStateToProps, mapDispatchToProps)(withStyles(styles))(SingleTutor)
 
   export default compose(
     withStyles(styles, {
