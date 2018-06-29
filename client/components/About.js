ca39eadeb0cf7b2b156171dd67a17b3171f91366
Allan James Lapid <ajLapid718@gmail.com>
2018-06-29 16:14:04 -0400

    (CREATE): Set up rough About view

---
+++ client/components/About.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <div id="about-parent-container">
+      <div id="about-title">
+        <h1>About Trinsic</h1>
+      </div>
+      <div>
+        <p>Trinsic is a peer-to-peer, skill-sharing platform built with blockchain technology.</p>
+        <p>With Trinsic, people are able to connect together and exchange skills with one another, creating a community centered around teaching and learning.</p>
+      </div>
+    </div>
+  );
+};
+
+export default About;
