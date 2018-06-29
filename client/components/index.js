36be55106410ca986e151853ec75578f64db0f55
Allan James Lapid <ajLapid718@users.noreply.github.com>
2018-06-29 16:38:06 -0400

    (CREATE): Set up rough About view (#59)

--- client/components/index.js
+++ client/components/index.js
@@ -1,8 +1,3 @@
-/**
- * `components/index.js` exists simply as a 'central export' for our components.
- * This way, we can import all of our components from the same place, rather than
- * having to figure out which file they belong to!
- */
 export {default as Navbar} from './navbar'
 export {default as UserHome} from './user-home'
 export {Login, Signup} from './auth-form'
@@ -12,3 +7,4 @@ export {default as ChatRoom} from './ChatRoom'
 export {default as SingleTutor} from './SingleTutor'
 export {default as AllTutors} from './AllTutors'
 export {default as SideBar} from './SideBar'
+export {default as About} from './About'
