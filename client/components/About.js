e191f53ed39e8a9f86b2b7c5c674bc817ed0aa81
Allan James Lapid <ajLapid718@gmail.com>
2018-06-30 21:37:45 -0400

    (STYLE): Basic styling

--- client/components/About.js
+++ client/components/About.js
@@ -3,15 +3,26 @@ import React from 'react'
 const About = () => {
   return (
     <div id="about-parent-container">
+
       <div id="about-title">
         <h1>About Trinsic</h1>
       </div>
-      <div>
-        <p>Trinsic is a peer-to-peer, skill-sharing platform built with blockchain technology.</p>
-        <p>With Trinsic, people are able to connect together and exchange skills with one another, creating a community centered around teaching and learning.</p>
+
+      <div id="about-description">
+        <p>Trinsic is a peer-to-peer, skill-sharing platform featuring blockchain-based smart contracts.</p>
+        <p>With Trinsic, people are able to connect together and exchange their expertise with one another.</p>
+        <p>The four pillars of Trinsic are: accessible education, transparency, credibility, and stewardship.</p>
+        <p>Our mission is to build a community centered around teaching and learning as we help contribute to opportunities for personal and professional growth.</p>
       </div>
+
+      <div id="about-team-members">
+        <div id="about-team-members-title">
+          <h1>Team</h1>
+        </div>
+      </div>
+
     </div>
-  );
+  )
 };
 
 export default About;
